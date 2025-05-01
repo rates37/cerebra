7217c49cffe58d68ca41b63dafb01ca0a13e5baa
Satya Jhaveri <satyajhaveri@protonmail.ch>
2025-05-02 09:58:30 +1000

    Refactor network classes

--- py/network.py
+++ py/network.py
@@ -1,75 +1,49 @@
 import numpy as np
 import graph
 from abc import ABC, abstractmethod
-from graph import Node, Operation
+from graph import Node, Variable
+from typing import Union, Optional, Any, List
 
 
-class Parameter(Node):
-    def __init__(self, value, name=None):
-        super().__init__(np.array(value), parents=[], op=None, name=name)
+class Parameter(Variable):
+    def __init__(self, value: Union[np.ndarray, float, int], name: Optional[str] = None) -> None:
+        super().__init__(value, name=name)
 
 
 class Module(ABC):
-    def __init__(self):
-        self._parameters = {}
-        self._modules = {}
-
-    def parameters(self) -> list[Parameter]:
-        parameters = []
-        # loop over all object attributes
-        # todo: find a cleaner way to do this if it exists
-        for attribute in self.__dict__.values():
-            if isinstance(attribute, Parameter):
-                parameters.append(attribute)
-            elif isinstance(attribute, Module):
-                parameters.extend(attribute.parameters())
-
-        return parameters
-
-    def __call__(self, *args, **kwargs):
-        args = [graph.to_node(a) for a in args]
-        return self.forward(*args, **kwargs)
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        return self.forward(*args, **kwds)
 
     @abstractmethod
-    def forward(self, *args, **kwargs):
+    def forward(self, *args: Any, **kwargs: Any) -> Any:
         pass
+
+    def parameters(self) -> List[Parameter]:
+        params = []
+        for attribute in self.__dict__.values():
+            if isinstance(attribute, Parameter):
+                params.append(attribute)
+            elif isinstance(attribute, Module):
+                params.extend(attribute.parameters())
+        return params
 
 
 class Linear(Module):
-    """
-    Simple FC Layer
-    """
-
-    def __init__(self, in_features, out_features, bias=True):
+    def __init__(self, in_features: int, out_features: int, bias: bool = True) -> None:
         super().__init__()
+        # Normal Xavier Initialisation
         self.weight = Parameter(
-            np.random.randn(in_features, out_features) * np.sqrt(2 / in_features),
+            np.random.randn(in_features, out_features) * np.sqrt(2 / (in_features+out_features)),
+            name="weight"
         )
+
         if bias:
-            self.bias = Parameter(np.zeros(out_features))
+            self.bias = Parameter(np.zeros(out_features), name="bias")
         else:
             self.bias = None
 
-    def forward(self, x):
-        if self.bias:
+    def forward(self, x: Node) -> Node:
+        if self.bias is not None:
             return (x @ self.weight) + self.bias
         else:
-            return x @ self.weight
-
-
-# Relu operation:
-class ReLU(Operation):
-    def forward(self, x):
-        return np.maximum(x, 0)
-
-    def backward(self, grad_output, node):
-        x_val = node.parents[0].value
-        grad = grad_output * (x_val > 0)
-        return [grad]
-
-
-def relu(x):
-    x = graph.to_node(x)
-    op = ReLU()
-    value = op.forward(x.value)
-    return Node(value, parents=[x], op=op)
+            return (x @ self.weight)
