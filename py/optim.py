14ec3ded28c71919139d6a8630aa2aab012143ad
Satya Jhaveri <satyajhaveri@protonmail.ch>
2025-05-02 10:04:13 +1000

    Add simple SGD optimiser

---
+++ py/optim.py
@@ -0,0 +1,17 @@
+from graph import Parameter
+from typing import List
+import numpy as np
+
+
+class SGD:
+    def __init__(self, parameters: List[Parameter], lr: float = 0.01) -> None:
+        self.parameters = parameters
+        self.lr = lr
+    
+    def step(self) -> None:
+        for p in self.parameters:
+            p.value = p.value - self.lr * p.grad
+    
+    def zero_grad(self) -> None:
+        for p in self.parameters:
+            p.grad = np.zeros_like(p.value)
