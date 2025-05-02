a2f46711c55e576f64a43d9b23142c78f3645437
Satya Jhaveri <satyajhaveri@protonmail.ch>
2025-05-02 18:28:43 +1000

    Add CE loss function to

--- py/network.py
+++ py/network.py
@@ -1,7 +1,6 @@
 import numpy as np
-import graph
 from abc import ABC, abstractmethod
-from graph import Node, Variable
+from graph import Node, Variable, Operation, to_node
 from typing import Union, Optional, Any, List
 
 
@@ -47,3 +46,37 @@ class Linear(Module):
             return (x @ self.weight) + self.bias
         else:
             return (x @ self.weight)
+
+
+class CrossEntropyLoss(Operation):
+    def __init__(self, target: np.ndarray) -> None:
+        self.target = target
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        # presume x in format: (batch_size, num_classes)
+        x_cpy = x.copy()
+        x_max = np.max(x_cpy, axis=1, keepdims=True)
+        x_exp = np.exp(x_cpy - x_max)
+        sum_exp = np.sum(x_exp, axis=1, keepdims=True)
+        self.softmax = x_exp / sum_exp
+        batch_size = x_cpy.shape[0]
+        log_probabilities = np.log(self.softmax[np.arange(
+            batch_size), self.target] + 1e-12)  # prevent instabilities
+        loss_val = -np.mean(log_probabilities)
+        return np.array([loss_val])
+
+    def backward(self, output_grad: np.ndarray, node: Node) -> List[np.ndarray]:
+        batch_size, _ = self.softmax.shape
+        one_hot_logits = np.zeros_like(self.softmax)
+        one_hot_logits[np.arange(batch_size), self.target] = 1.0
+        grad = (self.softmax - one_hot_logits) / batch_size
+        grad = grad * output_grad
+        return [grad]
+
+
+# function for CELoss so can be used as functional block:
+def cross_entropy_loss(x: Union[Node, np.ndarray, float, int], target: np.ndarray) -> Node:
+    x = to_node(x)
+    op = CrossEntropyLoss(target)
+    val = op.forward(x.value)
+    return Node(val, parents=[x], op=op)
