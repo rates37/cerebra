d801986cb55110aa31349bef3254e2cf6f7b1206
Satya Jhaveri <satyajhaveri@protonmail.ch>
2025-08-16 17:47:45 +1000

    Add backward tests for maxpool 2d
    Based on structure in the conv2d tests file

--- tests/test_pool2d.py
+++ tests/test_pool2d.py
@@ -1,13 +1,54 @@
 import unittest
 import numpy as np
+from typing import Callable, Optional
 from cerebra import Node, MaxPool2DOp, MaxPool2D
 
 EPSILON = 1e-6
 
 
+def numerical_gradient(func: Callable[[None], Node], node: Node, h=1e-6) -> np.ndarray:
+    """Compute gradient of node w.r.t. input
+
+    Args:
+        func (_type_): Callable that when invoked, re-evaluates computational graph
+                        based on the current state of `node`.
+        node (Node): The node whose value attribute will be altered to calculate the
+                        gradient
+        h (_type_, optional): Small perturbation value. Defaults to 1e-6.
+
+    Returns:
+        np.ndarray: Numpy array representing the numerically calculated gradient. Of same 
+                        shape as `node.value`.
+    """
+    input_value = node.value.copy()
+    grad = np.zeros_like(input_value, dtype=np.float64)
+
+    for i in np.ndindex(input_value.shape):
+        val = input_value[i]
+
+        # calculate f(x+h):
+        x_plus_h = input_value.copy()
+        x_plus_h[i] = val + h
+        node.value = x_plus_h
+        f_x_plus_h = func().value.item()  # func always returns a scalar (eg total loss)
+
+        # calculate f(x-h):
+        x_minus_h = input_value.copy()
+        x_minus_h[i] = val - h
+        node.value = x_minus_h
+        f_x_minus_h = func().value.item()
+
+        # central difference method:
+        grad[i] = (f_x_plus_h - f_x_minus_h) / (2 * h)
+
+    # restore input value of node
+    node.value = input_value
+    return grad
+
+
 class TestPool2d(unittest.TestCase):
     def setUp(self) -> None:
-        pass
+        self.default_rng = np.random.default_rng(69)
 
     def tearDown(self) -> None:
         pass
@@ -103,3 +144,54 @@ class TestPool2d(unittest.TestCase):
                ]]], dtype=np.float32)
 
         self.assertTrue(np.allclose(out.value, expected_out, atol=EPSILON))
+
+    #! ==========================
+    #!    Backward Pool Tests
+    #! ==========================
+    def _check_maxpool_op_backward(
+        self,
+        op: MaxPool2DOp,
+        x_val: np.ndarray,
+        grad_val: Optional[np.ndarray] = None
+    ) -> None:
+        x_node = Node(x_val.copy())
+
+        # run forward pass
+        output_from_forward = op.forward(x_node.value)
+
+        # generate random loss if necessary
+        if grad_val is None:
+            grad_val = self.default_rng.random(
+                output_from_forward.shape).astype(np.float64)
+
+        # dummy output node for backward call
+        dummy_output = Node(output_from_forward, parents=[x_node], op=op)
+        grads = op.backward(grad_val, dummy_output)
+        dx_analytical = grads[0]
+
+        # define function for numerical gradient calculation
+        def get_loss_as_node() -> Node:
+            out_val = op.forward(x_node.value)
+            loss_val = (out_val * grad_val).sum()
+            return Node(np.array([loss_val]))
+
+        dx_numerical = numerical_gradient(get_loss_as_node, x_node)
+        self.assertTrue(np.allclose(
+            dx_analytical, dx_numerical, atol=EPSILON, rtol=EPSILON))
+
+    def test_maxpool2d_op_backward(self):
+        op = MaxPool2DOp(kernel_size=(2, 2), stride=1, padding=0)
+        x_val = self.default_rng.random((1, 1, 4, 4)).astype(np.float64)
+        self._check_maxpool_op_backward(op, x_val)
+
+    def test_maxpool2d_op_backward_stride_padding_multichannel(self):
+        N = 2
+        C = 3
+        H = W = 5
+        kh = kw = 3
+        stride = 2
+        padding = 1
+
+        op = MaxPool2DOp(kernel_size=(kh, kw), stride=stride, padding=padding)
+        x_val = self.default_rng.random((N, C, H, W)).astype(np.float64)
+        self._check_maxpool_op_backward(op, x_val)
