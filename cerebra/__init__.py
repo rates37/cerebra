c891132c031dc60a89cfb9ef07e463dec46861e7
Satya Jhaveri <satyajhaveri@protonmail.ch>
2025-08-16 17:22:29 +1000

    Update module import/exports

--- cerebra/__init__.py
+++ cerebra/__init__.py
@@ -1,3 +1,3 @@
-from .network import cross_entropy_loss, Linear, Module, Parameter, Conv2d, Conv2dLayer
+from .network import cross_entropy_loss, Linear, Module, Parameter, Conv2d, Conv2dLayer, MaxPool2DOp, MaxPool2D
 from .graph import Variable, Operation, Node, relu
 from .optim import SGD
