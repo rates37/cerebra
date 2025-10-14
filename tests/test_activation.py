0a1ebf049eace53da642aef48c8d7bd075f925d0
Satya Jhaveri <satyajhaveri@protonmail.ch>
2025-10-14 23:36:49 +1100

    Add tanh tests

--- tests/test_activation.py
+++ tests/test_activation.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 from typing import Callable
-from cerebra import Node, relu, sigmoid
+from cerebra import Node, relu, sigmoid, tanh
 
 
 EPSILON = 1e-6
@@ -83,4 +83,40 @@ class TestActivationFunction(unittest.TestCase):
         y.backward(np.ones_like(y.value))
         expected_forward = 1.0 / (1.0 + np.exp(-x_vals))
         expected = expected_forward * (1.0 - expected_forward)
+        self.assertTrue(np.allclose(expected, x.grad, atol=EPSILON))
+
+    def test_tanh_forward(self) -> None:
+        x_vals = np.array([-3.0, -1.0, 0.0, 0.5, 2.0, 10.0], dtype=np.float64)
+        x = Node(x_vals)
+        expected = np.tanh(x_vals)
+
+        # test tanh on ndarray:
+        y = tanh(x_vals)
+        self.assertTrue(np.allclose(y.value, expected, atol=EPSILON))
+
+        # test tanh on Node:
+        y = tanh(x)
+        self.assertTrue(np.allclose(y.value, expected, atol=EPSILON))
+
+        # test tanh on int:
+        for x_val in x_vals:
+            x_val = int(x_val)
+            y = tanh(x_val)
+            expected = np.tanh(x_val)
+            self.assertTrue(np.allclose([y.value], [expected], atol=EPSILON))
+
+        # test tanh on floats:
+        for x_val in x_vals:
+            y = tanh((x_val))
+            expected = np.tanh(x_val)
+            self.assertTrue(np.allclose([y.value], [expected], atol=EPSILON))
+
+    def test_tanh_backward(self) -> None:
+        x_vals = np.array([-3.0, -1.0, 0.0, 0.5, 2.0, 10.0], dtype=np.float64)
+        x = Node(x_vals)
+
+        y = tanh(x)
+        y.backward(np.ones_like(y.value))
+        expected_forward = np.tanh(x_vals)
+        expected = 1 - expected_forward**2
         self.assertTrue(np.allclose(expected, x.grad, atol=EPSILON))
